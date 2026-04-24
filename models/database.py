888ce67ddefe62ce8214d014700e3d8f0ab37b64
rodion <rodionuvarov783@gmail.com>
2026-04-24 14:13:44 +0300

    docker update

--- models/database.py
+++ models/database.py
@@ -1,10 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
-SQLALCHEMY_DATABASE_URL='postgresql://postgres:123@localhost/blog_db'
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:123@localhost/blog_db")
 
-engine=create_engine(SQLALCHEMY_DATABASE_URL)
+engine=create_engine(DATABASE_URL)
 
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
