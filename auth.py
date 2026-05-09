648712e0c22fa45cdc8f93ba0ed653c2757a8c41
rodion <rodionuvarov783@gmail.com>
2026-05-09 21:55:28 +0300

    deploy update

--- auth.py
+++ auth.py
@@ -1,40 +1,42 @@
 import bcrypt
-from jose import JWTError,jwt
-from datetime import datetime,timedelta,timezone
-from fastapi import Depends,HTTPException,status
+from jose import JWTError, jwt
+from datetime import datetime, timedelta, timezone
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from models import models
-from main import get_db
-SECRET_KEY='your-secret-key-here-change-it'
-ALGORITHM='HS256'
-ACCESS_TOKEN_EXPIRE_MINUTES=30
 
-def hash_password(password:str):
-    if len(password.encode('utf-8'))>72:
-        password=password[:72]
+
+SECRET_KEY = 'your-secret-key-here-change-it'
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def hash_password(password: str):
+    if len(password.encode('utf-8')) > 72:
+        password = password[:72]
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt).decode('utf-8')
 
-def verify_password(plain_password:str,hashed_password:str)->bool:
-    if len(plain_password.encode('utf-8'))>72:
-        plain_password=plain_password[:72]
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    if len(plain_password.encode('utf-8')) > 72:
+        plain_password = plain_password[:72]
     return bcrypt.checkpw(
         plain_password.encode('utf-8'),
         hashed_password.encode('utf-8')
     )
-def create_access_token(data:dict,expires_delta:timedelta=None):
-    to_encode=data.copy()
-    if expires_delta:
-        expire=datetime.now(timezone.utc)+expires_delta
-    else:
-        expire=datetime.now(timezone.utc)+timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({'exp':expire})
-    encode_jwt=jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
-    return encode_jwt
 
-oauth2_scheme=OAuth2PasswordBearer(tokenUrl='login')
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({'exp': expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends("get_db")):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail='Could not validate credentials',
@@ -52,4 +54,3 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
     if user is None:
         raise credentials_exception
     return user
-
